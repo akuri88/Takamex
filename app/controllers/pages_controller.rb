4e5b4e260568358a425338a3dd6fdebc3e60b771
Adrian <akuri88@gmail.com>
2012-03-12 21:15:05 -0600

    Added basic layout

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def who
+  end
+
+  def what
+  end
+
+  def contact
+  end
+
+end
---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,14 @@
+module PagesHelper
+
+	def who_path
+		'/QuienesSomos'
+	end
+	
+	def what_path
+		'/QueHacemos'
+	end
+	
+	def contact_path
+		'/Contacto'
+	end
+end
