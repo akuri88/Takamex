291bd8633df38505625072a88d5ecd31e723f33e
Adrian <akuri88@gmail.com>
2012-03-12 18:58:39 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Takamex::Application.config.secret_token = 'c3f8d7f2873c7710872f477929e2d2a8b0b5836f15afc2d79b316e94c5200b0a977e89c436b9a9102f99025485b7a40515d5395689bf94ca4a1f659d911d1475'
